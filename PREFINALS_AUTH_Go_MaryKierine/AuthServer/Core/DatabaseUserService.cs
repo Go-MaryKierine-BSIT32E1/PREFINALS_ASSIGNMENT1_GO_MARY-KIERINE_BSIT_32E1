718cfc25131ebceb2fb2618c8880eaaaa16b1bdc
mary <mary@DESKTOP-57FEM11>
2024-05-03 13:30:27 +0800

    Add project files.

---
+++ PREFINALS_AUTH_Go_MaryKierine/AuthServer/Core/DatabaseUserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core
+{
+    public class DatabaseUserService : IUserService
+    {
+        public Task<User> RegisterUserAsync(string username, string password)
+        {
+            // Implement logic to register a new user in the database
+            // For example:
+            // - Connect to the database
+            // - Insert a new user record with the provided username and hashed password
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> AuthenticateUserAsync(string username, string password)
+        {
+            // Implement logic to authenticate a user against the database
+            // For example:
+            // - Connect to the database
+            // - Query the database to find a user with the provided username
+            // - Compare the hashed password with the provided password
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> ChangePasswordAsync(string username, string newPassword)
+        {
+            // Implement logic to change a user's password in the database
+            // For example:
+            // - Connect to the database
+            // - Update the user record with the new hashed password
+            throw new System.NotImplementedException();
+        }
+    }
+}
