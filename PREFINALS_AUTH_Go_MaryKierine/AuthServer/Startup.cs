d9622207e0508600e3ea27e333d3ebc863286aeb
Go-MaryKierine-BSIT32E1 <146524749+Go-MaryKierine-BSIT32E1@users.noreply.github.com>
2024-05-03 14:04:55 +0800

    Update Startup.cs

--- PREFINALS_AUTH_Go_MaryKierine/AuthServer/Startup.cs
+++ PREFINALS_AUTH_Go_MaryKierine/AuthServer/Startup.cs
@@ -20,7 +20,6 @@ namespace AuthServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -36,7 +35,6 @@ namespace AuthServer
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
