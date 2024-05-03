66de33b3b4348dd46ec24f0baf112778b527ab33
Go-MaryKierine-BSIT32E1 <146524749+Go-MaryKierine-BSIT32E1@users.noreply.github.com>
2024-05-03 14:03:42 +0800

    Update DatabaseUserService.cs

--- PREFINALS_AUTH_Go_MaryKierine/AuthServer/Core/DatabaseUserService.cs
+++ PREFINALS_AUTH_Go_MaryKierine/AuthServer/Core/DatabaseUserService.cs
@@ -9,29 +9,17 @@ namespace AuthServer.Core
     {
         public Task<User> RegisterUserAsync(string username, string password)
         {
-            // Implement logic to register a new user in the database
-            // For example:
-            // - Connect to the database
-            // - Insert a new user record with the provided username and hashed password
             throw new System.NotImplementedException();
         }
 
         public Task<User> AuthenticateUserAsync(string username, string password)
         {
-            // Implement logic to authenticate a user against the database
-            // For example:
-            // - Connect to the database
-            // - Query the database to find a user with the provided username
-            // - Compare the hashed password with the provided password
+        
             throw new System.NotImplementedException();
         }
 
         public Task<bool> ChangePasswordAsync(string username, string newPassword)
         {
-            // Implement logic to change a user's password in the database
-            // For example:
-            // - Connect to the database
-            // - Update the user record with the new hashed password
             throw new System.NotImplementedException();
         }
     }
